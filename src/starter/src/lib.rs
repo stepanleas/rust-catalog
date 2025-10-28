f926df5ca39c55f36599d333babd4678bb771f55
stepanleas <s.leas@developmentaid.org>
2025-10-28 11:48:54 +0200

    fix code formatting

--- src/starter/src/lib.rs
+++ src/starter/src/lib.rs
@@ -30,9 +30,9 @@ async fn run_internal(settings: &Settings) -> Result<Server> {
         settings: settings.clone(),
         category_repository: Arc::new(PostgresCategoryRepository::new(&pool)),
         product_repository: Arc::new(PostgresProductRepository::new(&pool)),
-        product_message_publisher: Arc::new(ProductKafkaMessagePublisher::new(
-            KafkaProducer::new(kafka_client)?,
-        )),
+        product_message_publisher: Arc::new(ProductKafkaMessagePublisher::new(KafkaProducer::new(
+            kafka_client,
+        )?)),
     };
 
     let server = HttpServer::new(move || {
